e4e4eb9b736f0b005c467750c827c88fc8ab58ed
StoicaAttila <stoicaattila99@gmail.com>
2023-05-31 20:38:56 +0300

    Made everything more visually pleasant, and added calculation box

--- dynamic-programming-visualization/src/components/solutions/MinCostBottomUpPath.tsx
+++ dynamic-programming-visualization/src/components/solutions/MinCostBottomUpPath.tsx
@@ -18,7 +18,11 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
 
     //Create container
     const container = document.createElement("div");
-    container.style.display = "flex";
+    container.classList.add("container");
+
+    // Create container for tables
+    const containerTable = document.createElement("div");
+    containerTable.classList.add("containerTable");
 
     // Create the table
     const table = document.createElement("table");
@@ -34,17 +38,23 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
 
     // Create the tableDP
     const tableDP = document.createElement("table");
-    tableDP.classList.add("tableDP");
     for (let i = 0; i < m; i++) {
         const rowDP = document.createElement("tr");
         for (let j = 0; j < n; j++) {
             const cellDP = document.createElement("td");
-            //cell.textContent = matrix[i][j].toString();
             cellDP.textContent = "0";
             rowDP.appendChild(cellDP);
         }
         tableDP.appendChild(rowDP);
     }
+
+    // Add the tables to the document
+    containerTable.appendChild(table);
+    containerTable.appendChild(tableDP);
+
+    // Create container for refresh button and calculation box
+    const buttonAndBox = document.createElement("div");
+    buttonAndBox.classList.add("buttonAndBox");
 
     // Create refresh button
     const refreshButton = document.createElement("button");
@@ -58,11 +68,18 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
     refreshButton.addEventListener("mouseout", function () {
         refreshButton.style.backgroundColor = "white";
     });
-    table.appendChild(refreshButton);
+    buttonAndBox.appendChild(refreshButton);
 
-    // Add the tables to the document
-    container.appendChild(table);
-    container.appendChild(tableDP);
+    // Create calculation box
+    const calculationBox = document.createElement("p");
+    calculationBox.textContent = "Calculation Box"
+    buttonAndBox.appendChild(calculationBox);
+
+    // Add tables, button and box to container
+    container.appendChild(containerTable);
+    container.appendChild(buttonAndBox);
+
+    // Add everything to the document
     document.body.appendChild(container);
 
     // Initialize the dp array with the first row and column
@@ -73,6 +90,7 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
 
     await new Promise((resolve) => setTimeout(resolve, delay));
     table.rows[0].cells[0].classList.add("search-path");
+    calculationBox.textContent = ` ${matrix[0][0].toString()}`
 
     await new Promise((resolve) => setTimeout(resolve, delay));
     table.rows[0].cells[0].classList.remove("search-path");
@@ -88,6 +106,8 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
         await new Promise((resolve) => setTimeout(resolve, delay));
         table.rows[0].cells[j].classList.add("search-path");
         tableDP.rows[0].cells[j - 1].classList.add("search-path");
+
+        calculationBox.textContent = ` ${matrix[0][j].toString()} + ${dp[0][j - 1].toString()}`
 
         await new Promise((resolve) => setTimeout(resolve, delay));
         table.rows[0].cells[j].classList.remove("search-path");
@@ -105,6 +125,8 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
         await new Promise((resolve) => setTimeout(resolve, delay));
         table.rows[i].cells[0].classList.add("search-path");
         tableDP.rows[i - 1].cells[0].classList.add("search-path");
+
+        calculationBox.textContent = ` ${matrix[i][0].toString()} + ${dp[i - 1][0].toString()}`
 
         await new Promise((resolve) => setTimeout(resolve, delay));
         table.rows[i].cells[0].classList.remove("search-path");
@@ -124,6 +146,9 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
             await new Promise((resolve) => setTimeout(resolve, delay));
             tableDP.rows[i - 1].cells[j].classList.add("search-path");
             tableDP.rows[i].cells[j - 1].classList.add("search-path");
+
+            calculationBox.textContent = ` Min(${dp[i - 1][j].toString()}, ${dp[i][j - 1].toString()})`
+
             await new Promise((resolve) => setTimeout(resolve, delay));
             tableDP.rows[i - 1].cells[j].classList.remove("search-path");
             tableDP.rows[i].cells[j - 1].classList.remove("search-path");
@@ -131,6 +156,9 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
 
             await new Promise((resolve) => setTimeout(resolve, delay));
             table.rows[i].cells[j].classList.add("search-path");
+
+            calculationBox.textContent = ` Min(${dp[i - 1][j].toString()}, ${dp[i][j - 1].toString()}) + ${matrix[i][j].toString()}`
+
             await new Promise((resolve) => setTimeout(resolve, delay));
             table.rows[i].cells[j].classList.remove("search-path");
             tableDP.rows[i].cells[j].textContent = dp[i][j].toString();
@@ -138,6 +166,8 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
             tableDP.rows[i].cells[j].classList.add("final-cost-path");
         }
     }
+
+    calculationBox.textContent = "-";
 
     // Find the minimum cost path
     const minCost = dp[m - 1][n - 1];
@@ -179,10 +209,7 @@ async function findMinCostBottomUpPath(delay: number): Promise<number> {
     }
 
     // Return the minimum cost path
-    const result = document.createTextNode("\nThe minimum cost path is: " + minCost + "\n");
-    const span = document.createElement("span");
-    span.appendChild(result);
-    document.body.appendChild(span);
+    calculationBox.textContent = ` The minimum cost path is: ${minCost.toString()}`
     return minCost;
 }
 
